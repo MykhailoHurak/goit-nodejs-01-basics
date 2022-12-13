4b7953d3289283a9c7192c22937a6d5a0400fd03
mikegurak <92752760+MykhailoHurak@users.noreply.github.com>
2022-12-13 07:56:51 +0200

    update

---
+++ node-module-CommonJS/index.js
@@ -0,0 +1,5 @@
+const users = require("./users");
+console.log(users);
+
+const {girls} = require("./users");
+// console.log(girls);
---
+++ node-module-CommonJS/users.js
@@ -0,0 +1,10 @@
+const boys = ["Mykhailo", "Andrii", "Serhii"];
+
+const girls = ["Liubov", "Vira", "Nadiia"];
+
+const users = {
+    boys,
+    girls
+};
+
+module.exports = users; // модуль для екпорта може бути лише 1 в файлі
