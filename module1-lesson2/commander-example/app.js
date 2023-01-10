b09836f06481d70ff4b83a596beb0459f53a48ad
mikegurak <92752760+MykhailoHurak@users.noreply.github.com>
2023-01-10 18:12:53 +0200

    update m1.2

---
+++ module1-lesson2/commander-example/app.js
@@ -0,0 +1,68 @@
+const { program } = require("commander");
+
+// const yargs = require("yargs");
+// const { hideBin } = require("yargs/helpers");
+
+const players = require("./players");
+
+const invokeAction = async ({ action, id, name, country }) => {
+    switch (action) {
+        case "all":
+            const allPlayers = await players.getAll();
+            console.log(allPlayers);
+            break;
+        case "getById":
+            const playerById = await players.getById(id);
+            console.log(playerById);
+            break;
+        case "add":
+            const addPlayer = await players.add({name, country});
+            console.log(addPlayer);
+            break;
+        case "updateById":
+            const updatePlayer = await players.update(id, {name, country});
+            console.log(updatePlayer);
+            break;
+        case "removeById":
+            const removePlayer = await players.remove(id);
+            console.log(removePlayer);
+            break;
+        default:
+            console.log("Unknown action");
+    }
+}
+
+// invokeAction({ action: "all" });
+// invokeAction({ action: "getById", id: "id-01q" });
+// invokeAction({ action: "add", name: "Modric", country: "Croatia" });
+// invokeAction({ action: "updateById", id: "j-0Q_lTWbnWDR84Wpa6Rk", name: "De Breine", country: "Belgium" });
+// invokeAction({ action: "removeById", id: "6J3ldoxrNO_wnf9dNs9JG"});
+
+// console.log(process.argv);
+
+// const actionIndex = process.argv.indexOf("--action");
+// if (actionIndex !== -1) {
+//     const action = process.argv[actionIndex + 1];
+//     invokeAction({ action });
+// }
+
+// ===============================================
+
+// const arr = hideBin(process.argv);
+// // console.log(arr);
+// const { argv } = yargs(arr);
+// // console.log(argv);
+// invokeAction(argv);
+
+// ===============================================
+
+program
+    .option("-a, --action <type>")
+    .option("-i, --id <type>")
+    .option("-n, --name <type>")
+    .option("-c, --country <type>");
+
+program.parse(); // шукає  process.argv і перетворює масив на обєкт
+
+const options = program.opts(); // повертає нам цей обєкт
+console.log(options);
---
+++ module1-lesson2/process.argv-example/app.js
@@ -0,0 +1,42 @@
+const players = require("./players");
+
+const invokeAction = async ({ action, id, name, country }) => {
+    switch (action) {
+        case "all":
+            const allPlayers = await players.getAll();
+            console.log(allPlayers);
+            break;
+        case "getById":
+            const playerById = await players.getById(id);
+            console.log(playerById);
+            break;
+        case "add":
+            const addPlayer = await players.add({name, country});
+            console.log(addPlayer);
+            break;
+        case "updateById":
+            const updatePlayer = await players.update(id, {name, country});
+            console.log(updatePlayer);
+            break;
+        case "removeById":
+            const removePlayer = await players.remove(id);
+            console.log(removePlayer);
+            break;
+        default:
+            console.log("Unknown action");
+    }
+}
+
+// invokeAction({ action: "all" });
+// invokeAction({ action: "getById", id: "id-01q" });
+// invokeAction({ action: "add", name: "Modric", country: "Croatia" });
+// invokeAction({ action: "updateById", id: "j-0Q_lTWbnWDR84Wpa6Rk", name: "De Breine", country: "Belgium" });
+// invokeAction({ action: "removeById", id: "6J3ldoxrNO_wnf9dNs9JG"});
+
+console.log(process.argv);
+
+const actionIndex = process.argv.indexOf("--action");
+if (actionIndex !== -1) {
+    const action = process.argv[actionIndex + 1];
+    invokeAction({ action });
+}
